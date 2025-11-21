ffb27594a3ff515c383853429bbb9d9b0174e0da
陳依琳 <im1010ioio@gmail.com>
2025-11-21 19:10:08 +0800

    Update member.js

--- src/member.js
+++ src/member.js
@@ -6,6 +6,10 @@ import { MemberProfile } from './components/MemberProfile';
 
 document.querySelector('#app').innerHTML = `
   ${Header()}
+  
+  <!-- Spacer for fixed header -->
+  <div class="h-24 bg-[#4A4A4A]"></div>
+
   <main>
     ${MemberProfile()}
   </main>
